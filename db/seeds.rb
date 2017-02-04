97b2564a72064dea8427568fcbdf3dd8bfc514c3
Csaba Nemeth <csaba.nemeth@csabanemeth.org>
2017-02-04 13:35:15 -0800

    Added more stuff to seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Owner
-owner = Owner.where(ssn: '123456789').first_or_create!
+owner1 = Owner.where(ssn: '123456789').first_or_create!
+owner2 = Owner.where(ssn: '987654321').first_or_create!
 
 # Property
-property = Property.where(owner: owner).first_or_initialize
-property.value = 10000000
-property.save!
+property1 = Property.where(owner: owner1).first_or_initialize
+property1.value = 10000000
+property1.save!
+
+property2 = Property.where(owner: owner2).first_or_initialize
+property2.value = 20000000
+property2.save!
 
 # Loan
-loan = Loan.where(owner: owner, property: property, status: 'pending').first_or_initialize
-loan.amount = 1000000
-loan.save!
+loan1 = Loan.where(owner: owner1, property: property1, status: 'pending').first_or_initialize
+loan1.amount = 1000000
+loan1.save!
+
+loan2 = Loan.where(owner: owner2, property: property2, status: 'pending').first_or_initialize
+loan2.amount = 5000000
+loan2.save!
